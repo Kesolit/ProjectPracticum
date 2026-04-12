bf89c340eba87b18cd97efeb064fde1d899dd5e1
LenL1n <lena_mokina@bk.ru>
2026-04-12 19:56:55 +0500

    добавление страницы регистрации

--- portfolio-builder-fe/src/App.tsx
+++ portfolio-builder-fe/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Editor from './pages/Editor/Editor'
+import Register from './pages/Register/Register'
 
 function App() {
-  return <Editor />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Editor />} />
+        <Route path="/register" element={<Register />} />
+        {/* TODO: добавить /login */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
