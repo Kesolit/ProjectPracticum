137c1ea0572973dc1198d75e123e0c900f9b9da2
Kira <kirilldidenko2007@gmail.com>
2026-05-16 18:19:56 +0500

    Update App.tsx

--- portfolio-builder-fe/src/App.tsx
+++ portfolio-builder-fe/src/App.tsx
@@ -2,17 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Editor from './pages/Editor/Editor'
 import Register from './pages/Register/Register'
 import Login from './pages/Login/Login'
-import PublicView from './pages/PublicView/PublicView' 
+import PublicView from './pages/PublicView/PublicView'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Preview from './pages/Preview/Preview'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Editor />} />
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        <Route path="/preview" element={<Preview />} />
+
+        <Route path="/editor/:id" element={<Editor />} />
+        
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         
-        {/* Исправлено: теперь здесь используется slug вместо id */}
         <Route path="/view/:slug" element={<PublicView />} />
       </Routes>
     </BrowserRouter>
